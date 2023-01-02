cd74a5d3cf486f1107714986d96b4e0a9eccf4e1
milad.xsar23 <milad.xsar72@gmail.com>
2023-01-02 11:28:23 +0330

    edit user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,11 @@
-//Factpry pattern
+//Factory pattern
 const user = (sequelize, DataTypes) => {
   const User = sequelize.define("user", {
+    id: {
+      type: DataTypes.UUIDV4,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     username: {
       type: DataTypes.STRING,
       defaultValue: "milad.xsar23",
